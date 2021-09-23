524bd03eca4a5127365e501caf27da1aa67517ec
Walter Lucetti <walter.lucetti@gmail.com>
2021-09-23 23:27:52 +0200

    Container and LifeCycle working

--- ldrobot_lidar_component/ldlidar_component/include/ldlidar_component.hpp
+++ ldrobot_lidar_component/ldlidar_component/include/ldlidar_component.hpp
@@ -1,12 +1,13 @@
 #ifndef LDLIDAR_COMPONENT_HPP
 #define LDLIDAR_COMPONENT_HPP
 
+#include <rcutils/logging_macros.h>
+
 #include "lifecycle_msgs/msg/transition.hpp"
 #include "rclcpp/publisher.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_lifecycle/lifecycle_node.hpp"
 #include "rclcpp_lifecycle/lifecycle_publisher.hpp"
-#include "rcutils/logging_macros.h"
 #include "visibility_control.hpp"
 
 namespace lc = rclcpp_lifecycle;
@@ -16,42 +17,46 @@ namespace ldlidar
 {
 class LdLidarComponent : public rclcpp_lifecycle::LifecycleNode
 {
+public:
   /// \brief Default constructor
   LDLIDAR_COMPONENTS_EXPORT
-  explicit LdLidarComponent(const std::string &node_name, const std::string &ns,
-                            const rclcpp::NodeOptions &options = rclcpp::NodeOptions())
-    : rclcpp_lifecycle::LifecycleNode(node_name, ns, options)
-  {
-  }
+  explicit LdLidarComponent(const rclcpp::NodeOptions &options = rclcpp::NodeOptions());
 
-    virtual ~LdLidarComponent();
+  virtual ~LdLidarComponent();
 
   /// \brief Callback from transition to "configuring" state.
   /// \param[in] state The current state that the node is in.
-  LNI::CallbackReturn on_configure(const lc::State & state) override;
+  LNI::CallbackReturn on_configure(const lc::State& prev_state) override;
 
   /// \brief Callback from transition to "activating" state.
   /// \param[in] state The current state that the node is in.
-  LNI::CallbackReturn on_activate(const lc::State & state) override;
+  LNI::CallbackReturn on_activate(const lc::State& prev_state) override;
 
   /// \brief Callback from transition to "deactivating" state.
   /// \param[in] state The current state that the node is in.
-  LNI::CallbackReturn on_deactivate(const lc::State & state) override;
+  LNI::CallbackReturn on_deactivate(const lc::State& prev_state) override;
 
   /// \brief Callback from transition to "unconfigured" state.
   /// \param[in] state The current state that the node is in.
-  LNI::CallbackReturn on_cleanup(const lc::State & state) override;
+  LNI::CallbackReturn on_cleanup(const lc::State& prev_state) override;
 
   /// \brief Callback from transition to "shutdown" state.
   /// \param[in] state The current state that the node is in.
-  LNI::CallbackReturn on_shutdown(const lc::State & state) override;
+  LNI::CallbackReturn on_shutdown(const lc::State& prev_state) override;
 
   /// \brief Callback from transition to "error" state.
   /// \param[in] state The current state that the node is in.
-  LNI::CallbackReturn on_error(const lc::State & state) override;
+  LNI::CallbackReturn on_error(const lc::State& prev_state) override;
+
+protected:
+  template <typename T>
+  void getParam(std::string paramName, T defValue, T &outVal, std::string log_info = std::string());
 
 private:
-}
+  // ----> Parameters
+  bool mDebugMode = true;
+  // <---- Parameters
+};
 
 }  // namespace ldlidar
 
--- ldrobot_lidar_component/ldlidar_component/src/ldlidar_component.cpp
+++ ldrobot_lidar_component/ldlidar_component/src/ldlidar_component.cpp
@@ -2,7 +2,111 @@
 
 namespace ldlidar
 {
+LdLidarComponent::LdLidarComponent(const rclcpp::NodeOptions& options)
+  : rclcpp_lifecycle::LifecycleNode("lidar_node", options)
+{
+  RCLCPP_INFO(get_logger(), "****************************************");
+  RCLCPP_INFO(get_logger(), " LDRobot DToF Lidar Lifecycle Component ");
+  RCLCPP_INFO(get_logger(), "****************************************");
+  RCLCPP_INFO(get_logger(), " * namespace: %s", get_namespace());
+  RCLCPP_INFO(get_logger(), " * node name: %s", get_name());
+  RCLCPP_INFO(get_logger(), "****************************************");
+
+  // ----> Parameters initialization
+  getParam("general.debug_mode", mDebugMode, mDebugMode);
+  if (mDebugMode)
+  {
+    rcutils_ret_t res = rcutils_logging_set_logger_level(get_logger().get_name(), RCUTILS_LOG_SEVERITY_DEBUG);
+
+    if (res != RCUTILS_RET_OK)
+    {
+      RCLCPP_INFO(get_logger(), "Error setting DEBUG level fot logger");
+    }
+    else
+    {
+      RCLCPP_INFO(get_logger(), "*** Debug Mode enabled ***");
+    }
+  }
+  else
+  {
+    rcutils_ret_t res = rcutils_logging_set_logger_level(get_logger().get_name(), RCUTILS_LOG_SEVERITY_INFO);
+
+    if (res != RCUTILS_RET_OK)
+    {
+      RCLCPP_INFO(get_logger(), "Error setting INFO level for logger");
+    }
+  }
+
+  RCLCPP_DEBUG(get_logger(), "[ROS2] Using RMW_IMPLEMENTATION = %s", rmw_get_implementation_identifier());
+  // <---- Parameters initialization
 }
+
+LdLidarComponent::~LdLidarComponent()
+{
+}
+
+template <typename T>
+void LdLidarComponent::getParam(std::string paramName, T defValue, T& outVal, std::string log_info)
+{
+  declare_parameter(paramName, rclcpp::ParameterValue(defValue));
+
+  if (!get_parameter(paramName, outVal))
+  {
+    RCLCPP_WARN_STREAM(get_logger(), "The parameter '"
+                                         << paramName << "' is not available or is not valid, using the default value: "
+                                         << defValue);
+  }
+
+  if (!log_info.empty())
+  {
+    RCLCPP_INFO_STREAM(get_logger(), log_info << outVal);
+  }
+}
+
+LNI::CallbackReturn LdLidarComponent::on_configure(const lc::State& prev_state)
+{
+  RCLCPP_DEBUG_STREAM(get_logger(),
+                      "on_configure: " << prev_state.label() << " [" << static_cast<int>(prev_state.id()) << "] -> Inactive");
+  return LNI::CallbackReturn::SUCCESS;
+}
+
+LNI::CallbackReturn LdLidarComponent::on_activate(const lc::State& prev_state)
+{
+  RCLCPP_DEBUG_STREAM(get_logger(),
+                      "on_activate: " << prev_state.label() << " [" << static_cast<int>(prev_state.id()) << "] -> Active");
+  return LNI::CallbackReturn::SUCCESS;
+}
+
+LNI::CallbackReturn LdLidarComponent::on_deactivate(const lc::State& prev_state)
+{
+  RCLCPP_DEBUG_STREAM(get_logger(),
+                      "on_deactivate: " << prev_state.label() << " [" << static_cast<int>(prev_state.id()) << "] -> Inactive");
+  return LNI::CallbackReturn::SUCCESS;
+}
+
+LNI::CallbackReturn LdLidarComponent::on_cleanup(const lc::State& prev_state)
+{
+  RCLCPP_DEBUG_STREAM(get_logger(),
+                      "on_cleanup: " << prev_state.label() << " [" << static_cast<int>(prev_state.id()) << "] -> Unconfigured");
+  return LNI::CallbackReturn::SUCCESS;
+}
+
+LNI::CallbackReturn LdLidarComponent::on_shutdown(const lc::State& prev_state)
+{
+  RCLCPP_DEBUG_STREAM(get_logger(),
+                      "on_shutdown: " << prev_state.label() << " [" << static_cast<int>(prev_state.id()) << "] -> Finalized");
+  RCLCPP_INFO_STREAM("Node is entered in the 'Finalized' state. Press Ctrl+C to kill...")
+  return LNI::CallbackReturn::SUCCESS;
+}
+
+LNI::CallbackReturn LdLidarComponent::on_error(const lc::State& prev_state)
+{
+  RCLCPP_DEBUG_STREAM(get_logger(),
+                      "on_error: " << prev_state.label() << " [" << static_cast<int>(prev_state.id()) << "] -> Finalized");
+  return LNI::CallbackReturn::SUCCESS;
+}
+
+}  // namespace ldlidar
 
 #include <rclcpp_components/register_node_macro.hpp>  // NOLINT
 RCLCPP_COMPONENTS_REGISTER_NODE(ldlidar::LdLidarComponent)
