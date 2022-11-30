c3fc2b64ba2d90b544ddaa21bd352fa13ff6209c
Walter Lucetti <walter.lucetti@stereolabs.com>
2022-11-30 23:04:01 +0100

    Fix launch formatting

--- ldlidar_node/launch/ldlidar_rviz2.launch.py
+++ ldlidar_node/launch/ldlidar_rviz2.launch.py
@@ -16,13 +16,11 @@
 import os
 
 from ament_index_python.packages import get_package_share_directory
-
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
-from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 
 def generate_launch_description():
